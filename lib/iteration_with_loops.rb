5047568fe800303516ae6b4f4313a0cb97fb93f7
Felix Rodriguez <frod25@gmail.com>
2020-04-28 21:26:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(src)
    src.length.times { |idx|
      string = ""
      src[idx].length.times {|idx2|
-       if string + src[idx][idx2]
+       if src[idx][idx2].is_a? string
          string = string + " " + src[idx][idx2]
        end
      }
